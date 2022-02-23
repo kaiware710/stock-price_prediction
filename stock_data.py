231d17b901592a35599edebf62f31cf723882d24
kaiware710 <yu.sskabc.my123@gmail.com>
2022-02-23 18:22:10 +0900

    Remove line breaks after google docstring. Made the whole code easier to see.

--- stock_data.py
+++ stock_data.py
@@ -5,27 +5,21 @@ import pandas_datareader.data as pdr
 
 def get_stock_data(code):
     """Stooqから株価データ5年分をdfで取得
-
     Args:
         code (int): 銘柄コード
-
     Returns:
         df (pandas.core.frame.DataFrame): 株価データのDataFrame
     """
-
     df = pdr.DataReader("{}.JP".format(code), "stooq").sort_index()
-
     return df
 
 
 def stock_line_graph(df, column):
     """dfの特定列のデータの折れ線グラフを保存
-
     Args:
         df (pandas.core.frame.DataFrame): 株価データのDataFrame
         column (str): 列名
     """
-
     plt.figure()
     df[column].plot()
     plt.savefig("./graph/stock_close.png")
@@ -34,13 +28,11 @@ def stock_line_graph(df, column):
 
 def stock_candle_graph(df, number_of_data, mav):
     """ローソク足チャート、移動平均線、出来高を表示
-
     Args:
         df (pandas.core.frame.DataFrame): 株価データのDataFrame
         number_of_data (int): 可視化するデータ数（最新からの数）
         mav (tuple - int): {mav}日の移動平均線
     """
-
     mpf.plot(df.tail(number_of_data), type="candle", mav=mav, volume=True)
 
 
@@ -52,4 +44,4 @@ stock_data_df = get_stock_data(CODE)
 
 NUMBER_OF_DATA = 200
 MOVING_AVARAGE = (5, 25, 75)
-stock_candle_graph(stock_data_df, NUMBER_OF_DATA, MOVING_AVARAGE)
+# stock_candle_graph(stock_data_df, NUMBER_OF_DATA, MOVING_AVARAGE)
